6f2cd645bc222d483e13d383f69391b3d8507ef2
Vinod Pillai <vinod.pillai@heliossolutions.co>
2023-09-16 22:07:00 +0530

    Added end points to showcase the path parameter

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -7,7 +8,27 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-def sum(firstName : str, lastName : str):
-    return firstName + " " + lastName
+'''
+Path Parameters
+'''
+@app.get("/result/{value}")
+async def result_value(value : int):
+    return {"message":"We can have method with type and without also her the path parameters value is: "+ str(value)}
+
+'''
+Class
+'''
+class ModelName(str,Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
+'''Path Parameter Class object'''
+@app.get("/enum/{value}")
+async def get_model(model_name : ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name":model_name, "message":"Deep learning FTW!"}
+    if model_name.value == 'resnet':
+        return {"model_name":model_name, "message":"LeCNN all the images"}
+    return {"model_name":model_name, "message":"Have some residuals"}
